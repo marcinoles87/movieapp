101a8369b1435a07360fc5f70ea59fbdfd2a53cb
marcinoles87 <marcinoles87@gmail.com>
2023-12-29 15:27:20 +0100

    work with data movie

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import Movie from './components/Movie';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data , setData] = useState();
+  const [error , setError] = useState();
+
+  useEffect( () => {
+
+  }, [])
   return (
     <div className="App">
      
+     <Movie></Movie>
     </div>
   );
 }
