00aea816419d94b8ac31938f9187855627195745
marcinoles87 <marcinoles87@gmail.com>
2024-01-04 08:37:08 +0100

    work with button function

--- src/App.js
+++ src/App.js
@@ -29,13 +29,31 @@ function App() {
 
   }, [error])
 
+  let start = 0
+  let newX = -150
+  let startX = 0
+  let newButtonX = 150
 
+ const handleOnClick = () => {
+  
+  start+=newX
+  startX+=newButtonX
+  const movie = document.querySelector('.movies-data');
+  console.log(movie)
+  movie.style.transform = "translateX(" + start + "px)"
+  const button = document.querySelector('.movie-button');
+  button.style.transform = "translateX("+ newButtonX +" +px)"
+
+
+ 
+ }
 
   
   return (
     <div className="App">
      
      <Movie  dataPost={error ? postData : error} data={error ? data : error} error={error}></Movie>
+     <button className='movie-button' onClick={handleOnClick}> {`>`}</button>
     </div>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,24 +3,7 @@ import React from 'react'
 
 function Movie({dataPost , data , error}) {
 
-  let start = 0
-  let newX = -150
-  let startX = 0
-  let newButtonX = 150
 
- const handleOnClick = () => {
-  
-  start+=newX
-  startX+=newButtonX
-  const movie = document.querySelector('.movies-data');
-  console.log(movie)
-  movie.style.transform = "translateX(" + start + "px)"
-  const button = document.querySelector('.movie-button');
-  button.style.transform = "translateX("+ newButtonX +" +px)"
-
-
- 
- }
  
   return (
     <div className='movie-container'>
@@ -29,7 +12,7 @@ function Movie({dataPost , data , error}) {
         { data && dataPost ?  data.map( (movie , index) => {
           return(
           <div key={index} className='movie-item'>
-            <img src={movie.thumbnailUrl}></img>
+            <img src={movie.thumbnailUrl} alt={movie.title}></img>
             <h1>{movie.title}</h1>
             </div>
        ) })  : error }
@@ -37,7 +20,7 @@ function Movie({dataPost , data , error}) {
      
         
        
-       <button className='movie-button' onClick={handleOnClick}> {`>`}</button>
+      
         
       </div>
     </div>
