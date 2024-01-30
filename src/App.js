06b67d7e4674fdf6964d98544a87b77bc1aebee3
marcinoles87 <marcinoles87@gmail.com>
2024-01-30 15:55:22 +0100

    work with deploy

--- src/App.js
+++ src/App.js
@@ -39,8 +39,8 @@ function App() {
       
 
         console.log('dalej data bez ')
-      const URL = `http://www.omdbapi.com/?s=spider&apikey=5217a1e0&i&page=1`;
-      const URL2 = `http://www.omdbapi.com/?s=spider&apikey=5217a1e0&i&page=2`;
+      const URL = `https://www.omdbapi.com/?s=spider&apikey=5217a1e0&i&page=1`;
+      const URL2 = `https://www.omdbapi.com/?s=spider&apikey=5217a1e0&i&page=2`;
       const response = await fetch(URL);
       const response2 = await fetch(URL2);
       const finalData = await response.json();
@@ -56,7 +56,7 @@ function App() {
 
       if(newFilm === true){
         
-      const URL = `http://www.omdbapi.com/?s=${validMovie}&page=2&apikey=5217a1e0&i`;
+      const URL = `https://www.omdbapi.com/?s=${validMovie}&page=2&apikey=5217a1e0&i`;
       const response = await fetch(URL);
       const finalData = await response.json();
       console.log(finalData.Search)
